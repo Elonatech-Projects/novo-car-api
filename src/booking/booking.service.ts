ab3ef6c3ddc9d9c8c8de65e54cdd891b7c3ccafd
Elonatech's Projects <elonatechprojects@elonatech.com.ng>
2025-11-27 13:41:19 +0100

    All correction made

--- src/booking/booking.service.ts
+++ src/booking/booking.service.ts
@@ -1,6 +1,6 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model, Types } from 'mongoose';
+import { Model } from 'mongoose';
 import { Booking } from './schema/booking.schema';
 import { CreateBookingDto } from './dto/create-booking.dto';
 import { Auth } from '../auth/schema/auth-schema';
@@ -12,40 +12,36 @@ export class BookingService {
     @InjectModel(Auth.name) private userModel: Model<Auth>,
   ) { }
 
- async createBooking(userId: string, dto: CreateBookingDto) {
-  const { pickupDate, pickupLocation, pickupTime, shuttleType, passengers } = dto;
+   async createBooking(userId: string, dto: CreateBookingDto) {
+    const { pickupDate, pickupLocation, pickupTime, shuttleType, passengers } = dto;
 
-  const data = {
-    pickupDate,
-    pickupLocation,
-    pickupTime,
-    shuttleType,
-    passengers,
-  };
-
-  for (const [key, value] of Object.entries(data)) {
-    if (!value) {
-      throw new BadRequestException(`${key} is required`);
+    // Validate required fields
+    for (const [key, value] of Object.entries(dto)) {
+      if (!value) {
+        throw new BadRequestException(`${key} is required`);
+      }
     }
+
+    const user = await this.userModel.findById(userId);
+    if (!user) {
+      throw new BadRequestException(`User not found`);
+    }
+
+    const userBooking = {
+      pickupDate,
+      pickupLocation,
+      pickupTime,
+      shuttleType,
+      passengers,
+      userId: user._id, 
+    };
+
+    const userData = await this.bookingModel.create(userBooking);
+
+    return {
+      message: 'User booking created',
+      userData,
+    };
   }
-
-  const user = await this.userModel.findById(userId);
-
-  if (!user) {
-    throw new BadRequestException(`User not found`);
-  }
-
-  const userBooking = {
-    data,
-    user: user._id,
-  };
-
-  const userData = await this.bookingModel.create(userBooking);
-
-  return {
-    message: 'User booking created',
-    userData,
-  };
-}
 
 }
